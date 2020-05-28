274ec853e77b789f41a46ee2e9c4d6f1ea2d495e
lercat <ddbieres@gmail.com>
2020-05-28 17:38:09 +0200

    initialisation projet

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="assets/img/cheers.jpg" class="rounded-circle" alt="cheers">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h2>Beers O'Clock</h2></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
+            </div>
+
+            <div class="pt-4 font-weight-bold">Beers</div>
+            <div>Nous sommes une communauté de millions de buveurs de bières. Ce blog est open source pour tout ceux qui veulent apporter leur contribution (3) non profit ou pas !!!</div>
+            <div> <a href="#">www.ddbieres.com</a> </div>
+
+            <div class="row pt-5">
+                <div class="col-4">
+                    <img src="assets/img/barbar.png" class="w-100" alt="bière barbar">
+                </div>
+                <div class="col-4">
+                    <img src="assets/img/bernardus.png" class="w-100" alt="bière bernardus">
+                </div>
+                <div class="col-4">
+                    <img src="assets/img/erdinger.png" class="w-100" alt="bière erdinger">
+                </div>
+            </div>
+        </div>
+        
+
+
+    </div>
+</div>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>BeersOClock</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="assets/css/monstyle.css">
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Se connecter</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">S'inscrire</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Beers O'Clock
+                </div>
+                <div>
+                    <h2>Quoi de mieux qu'une petite mousse pour se détendre</h2>
+                </div>
+                <div>
+                    <img src="assets/img/tictac.jpg" alt="">
+                </div>
+                <div class="links">
+                    <a href="#">Mentions Légales</a>
+                    <a href="#">Contact</a>
+                    <a href="#">News</a>
+                    <a href="#">Le Blog</a>
+                    <a href="#">GitHub</a>
+                </div>
+            </div>
+        </div>
+    </body>
+
+</html>
